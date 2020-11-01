fb2dcb27a4be01c0c836538170a0f09b3f695616
Neal Haddaway <nealhaddaway@Neals-iMac.fritz.box>
2020-11-01 18:09:35 +0100

    Update with automated hyperlink recognition.

--- R/dataframe2html.R
+++ R/dataframe2html.R
@@ -5,7 +5,7 @@
 #' The HTML file also includes JavaScript functions to support URL suffix searching via 
 #' appending '?s=' and the search terms to the final URL. A JavaScript function filters 
 #' the table and displays matching rows (based on an exact match search across all 
-#' columns), updating the disaplyed row totals in the summary line.
+#' columns), updating the disaplyed row totals in the summary line. The function also
 #' @param data A dataframe containing columns and rows of data.
 #' @param titlefont_family Font family for the title text. Default is 'sans-serif'.
 #' @param titlefont_size Font size for the title text. Units must be specified. 
@@ -37,6 +37,10 @@
 #' changes in the input field to search for exact matches across all columns. Search 
 #' can also be performed by appending '?s=' and the search terms to the URL of the 
 #' final output HTML.
+#' @param hyperlinks If TRUE, the entire contents of any column with cell values 
+#' starting with 'www.' or http' are converted to hyperlink objects using '<a href="" 
+#' target="_blank">', directing the hyperlink to open in a new tab. Default is 'TRUE'. 
+#' If set to 'FALSE', hyperlinks are ignored and eill appear as plain text.
 #' @return an HTML object containing the full HTML code for the webpage. The function 
 #' also saves the HTML file to the working directory.
 #' @example 
@@ -53,7 +57,8 @@
 #'         "theor: some extra text", "refle: some extra text", "search: some extra text")
 #'     html <- dataframe2html(data,
 #'         tooltips = tooltips,
-#'         table_width = '6000px')
+#'         table_width = '6000px',
+#'         hyperlinks = 'TRUE')
 #' }
 #' @export
 dataframe2html <- function(data,
@@ -70,7 +75,8 @@ dataframe2html <- function(data,
                            text_align = 'left',
                            hoverbg_colour = '#f4f3ed',
                            other_CSS = '',
-                           search_bar = 'hidden'
+                           search_bar = 'hidden',
+                           hyperlinks = 'TRUE'
                            ){
   htmlleading <- paste('<!DOCTYPE html>\n <html>\n <head>\n \t<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>\n \t<meta name="viewport" content="width=device-width, initial-scale=2">\n \t\n \t<style type="text/css">\n .titletext {\n \tfont-weight: 400;\n \tfont-family: ',
                        titlefont_family,
@@ -105,6 +111,18 @@ dataframe2html <- function(data,
                        '>\n',
                        sep = '')
   htmltrailing <- '<script>\n// extracts text from trailing end of URL and enters it into the filter\nfunction prefill (){\n\tvar loc = location.href;\n\tvar locationField1 = document.getElementById("myInput1");\n\tvar printout = document.getElementById("selection");\n\tconst var1 = loc.split("?s=").pop();\n\tvar part1 = var1.substring(\n\t\t\tvar1.lastIndexOf("?s=") + 1, \n\t\t\tvar1.lastIndexOf("")\n\t);\n\tpart1 = part1.replace(/%20/g, " ");\n\tpart1 = part1.replace(/%2F/g, "/");\n\tif (loc.includes("?s=")) {\n\t\tlocationField1.value = part1;\n\t\tprintout.innerHTML = part1;\n\t} else {}\n}\n\n// filters rows according to input values\nfunction myFunction() {\n\tvar input, filter, table, tr, td, i;\n\tinput = document.getElementById("myInput1");\n\tfilter = input.value.toUpperCase();\n\ttable = document.getElementById("tbody_id");\n\ttr = table.getElementsByTagName("tr");\n\t\n\t// embedded code to calculate number of total rows in the table\n\tvar totcounts = document.getElementById("totcount");\n\tvar output = tr.length;\n\ttotcounts.innerHTML = output;\n\t\n\tfor (var i = 0; i < tr.length; i++) {\n\t\tvar tds = tr[i].getElementsByTagName("td");\n\t\tvar flag = false;\n\t\tfor(var j = 0; j < tds.length; j++){\n\t\t\tvar td = tds[j];\n\t\t\tif (td.innerHTML.toUpperCase().indexOf(filter) > -1) {\n\t\t\t\tflag = true;\n\t\t\t} \n\t\t}\n\t\tif(flag){\n\t\t\t\ttr[i].style.display = "";\n\t\t}\n\t\telse {\n\t\t\t\ttr[i].style.display = "none";\n\t\t}\n\t}\n}\n\n// creates a count of the number of rows shown in the filtered table\nfunction UpdateCount () {\n\tvar returnt = document.getElementById("rowscount");\n\tvar totalRows = $(\'#tbody_id tr:not([style*="display: none"])\').length;\nreturnt.innerHTML = totalRows;\n}\n</script>\n</body>\n</html>\n'
+  
+  if(hyperlinks == TRUE) {
+      data[,grep('www.|http', substr(data[1,], 1, 4))] <- gsub('\n', 
+                                                               '',
+                                                               paste('<a href="',
+                                                                     data[,grep('www.|http', substr(data[1,], 1, 4))],
+                                                                     '" target="_blank">',
+                                                                     data[,grep('www.|http', substr(data[1,], 1, 4))],
+                                                                     '</a>',
+                                                                     sep = '')
+                                                               )
+  }
   
   part1 <- paste('<table id="table_id" class="table_class">\n\t<thead id="thead_id" class="thead_class">\n\t\t<tr>\n',
                  if(length(tooltips) > 1){
